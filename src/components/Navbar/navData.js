e27101d11b730429b173282262cc3b22a01e73e3
Hsen-123 <hazimehussein01@gmail.com>
2024-01-23 18:56:17 +0200

    first commit

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,32 @@
+export const navLinks = [
+  {
+    label: 'Home',
+    pathname: '/',
+  },
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'About',
+    pathname: '/#about',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/hamishMW',
+    icon: 'twitter',
+  },
+  {
+    label: 'Figma',
+    url: 'https://www.figma.com/@Hamish',
+    icon: 'figma',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/HamishMW',
+    icon: 'github',
+  },
+];
---
+++ src/layouts/Home/Home.js
@@ -0,0 +1,120 @@
+import sliceTextureLarge from 'assets/proj2.png';
+import sliceTexturePlaceholder from 'assets/proj2.png';
+import sliceTexture from 'assets/proj2.png';
+import sprTextureLarge from 'assets/proj1.png';
+import sprTexturePlaceholder from 'assets/proj1.png';
+import sprTexture from 'assets/proj1.png';
+import { Footer } from 'components/Footer';
+import { Meta } from 'components/Meta';
+import { Intro } from 'layouts/Home/Intro';
+import { Profile } from 'layouts/Home/Profile';
+import { ProjectSummary } from 'layouts/Home/ProjectSummary';
+import { useEffect, useRef, useState } from 'react';
+import styles from './Home.module.css';
+
+const disciplines = ['Developer', 'Designer', 'Animator'];
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const projectOne = useRef();
+  const projectTwo = useRef();
+  const details = useRef();
+
+  useEffect(() => {
+    const sections = [intro, projectOne, projectTwo, details];
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      sectionObserver.observe(section.current);
+    });
+
+    indicatorObserver.observe(intro.current);
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, [visibleSections]);
+
+  return (
+    <div className={styles.home}>
+      <Meta
+        title="Designer + Developer"
+        description="Design portfolio of Hussein Hazime — a product designer working on web & mobile
+          apps with a focus on motion, experience design, and accessibility."
+      />
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        disciplines={disciplines}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <ProjectSummary
+        id="project-1"
+        sectionRef={projectOne}
+        visible={visibleSections.includes(projectOne.current)}
+        index={1}
+        title="Hebbosites Company Website"
+        description="Develop a customizable website that allows users to input their specific requirements and tailor the website accordingly."
+        buttonText="View project"
+        buttonLink="https://hebbosites.vercel.app"
+        model={{
+          type: 'laptop',
+          alt: 'Smart Sparrow lesson builder',
+          textures: [
+            {
+              srcSet: [sprTexture, sprTextureLarge],
+              placeholder: sprTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-2"
+        sectionRef={projectTwo}
+        visible={visibleSections.includes(projectTwo.current)}
+        index={2}
+        title="Mashabeat Music Website"
+        description="Develop a website where the Singer can upload and sell his songs and remixs."
+        model={{
+          type: 'laptop',
+          alt: 'Annotating a biomedical image in the Slice app',
+          textures: [
+            {
+              srcSet: [sliceTexture, sliceTextureLarge],
+              placeholder: sliceTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <Profile
+        sectionRef={details}
+        visible={visibleSections.includes(details.current)}
+        id="about"
+      />
+      <Footer />
+    </div>
+  );
+};
